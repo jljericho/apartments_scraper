1be4dff519648cfead7488b216efe6da4346b6c9
Jericho Locke <jljericho@gmail.com>
2023-03-03 13:32:50 -0500

    Added components for basic apartments

--- apartments/spiders/apartments_spider.py
+++ apartments/spiders/apartments_spider.py
@@ -30,8 +30,11 @@ class ApartmentsSpider(scrapy.Spider):
             "address": response.css("span.delivery-address").css("span::text").get() or property_name,
             "zip_code": zip_code,
             "state": state,
+            "price": response.xpath("//p[text()='Monthly Rent']/following-sibling::p[1]/text()").get(),
+            "bedrooms": response.xpath("//p[text()='Bedrooms']/following-sibling::p[1]/text()").get(),
+            "bathrooms": response.xpath("//p[text()='Bathrooms']/following-sibling::p[1]/text()").get(),
+            "square_feet": response.xpath("//p[text()='Square Feet']/following-sibling::p[1]/text()").get(),
             "neighborhood": response.css("a.neighborhood::text").get(),
-            "rent": response.css("p.rentInfoDetail::text").get(),
             "walk_score": response.css("div#walkScoreValue::text").get(),
             "transit_score": response.css("div.transitScore::attr(data-score)").get(),
             "bike_score": response.css("div.bikeScore::attr(data-score)").get(),
@@ -53,27 +56,31 @@ class ApartmentsSpider(scrapy.Spider):
                     })
 
         basic_info["parking"] = parking
-        for model in response.css("div.pricingGridItem"):
-            beds, bath, sqft_range = model.css("span.detailsTextWrapper span::text").getall()[:3]
+        if not response.css("div.pricingGridItem").get():
+            yield basic_info
+        else:
+            for model in response.css("div.pricingGridItem"):
+                beds, bath, sqft_range = model.css("span.detailsTextWrapper span::text").getall()[:3]
 
-            model_info = {
-                "model": model.css("span.modelName::text").get(),
-                "beds": beds,
-                "bath": bath
-            }
-            for unit in model.css("li.unitContainer"):
-                name_components = unit.css("button.unitBtn *::text").getall()
-                unit_name = "".join(name_components).strip(" \r\n")
-
-                unit_info = {
-                    "unit": unit_name,
-                    "price": "".join(unit.css("div.pricingColumn span *::text").getall()).replace("price ", "").strip(" \r\n"),
-                    "square_feet": "".join(unit.css("div.sqftColumn span *::text").getall()).replace("square feet ", "").strip(" \r\n"),
-                    "available": "".join(unit.css("span.dateAvailable *::text").getall()).replace("availibility ", "").strip(" \r\n"),
+                model_info = {
+                    "model": model.css("span.modelName::text").get(),
+                    "bedrooms": beds,
+                    "bathrooms": bath
                 }
-                unit_info.update(model_info)
-                unit_info.update(basic_info)
-                yield unit_info
+                for unit in model.css("li.unitContainer"):
+                    name_components = unit.css("button.unitBtn *::text").getall()
+                    unit_name = "".join(name_components).strip(" \r\n")
+
+                    unit_info = {
+                        "unit": unit_name,
+                        "price": "".join(unit.css("div.pricingColumn span *::text").getall()).replace("price ", "").strip(" \r\n"),
+                        "square_feet": "".join(unit.css("div.sqftColumn span *::text").getall()).replace("square feet ", "").strip(" \r\n"),
+                        "available": "".join(unit.css("span.dateAvailable *::text").getall()).replace("availibility ", "").strip(" \r\n"),
+                    }
+                    basic_info = basic_info.copy()
+                    basic_info.update(unit_info)
+                    basic_info.update(model_info)
+                    yield basic_info
 
 
 def _combine_css_texts(text_list: list[str]) -> str:
